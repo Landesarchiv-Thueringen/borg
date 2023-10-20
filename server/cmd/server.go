5b0e5be665e1001a4ae97cb08e97e47fad5d2f9c
Tony Grochow <tony.grochow@la.thueringen.de>
2023-10-20 17:55:45 +0200

    analyse file init

--- server/cmd/server.go
+++ server/cmd/server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 var defaultResponse = "borg server is running"
@@ -17,14 +18,34 @@ func main() {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"*"}
 	corsConfig.AllowHeaders = []string{"Origin", "Content-Type"}
-	corsConfig.AllowMethods = []string{"GET"}
-	// It's important that the cors configuration is used before declaring the routes
+	corsConfig.AllowMethods = []string{"GET", "POST"}
+	// It's important that the cors configuration is used before declaring the routes.
 	router.Use(cors.New(corsConfig))
 	router.GET("", getDefaultResponse)
+	router.POST("analyse-file", analyseFile)
 	addr := "0.0.0.0:" + os.Getenv("BORG_SERVER_CONTAINER_PORT")
 	router.Run(addr)
 }
 
 func getDefaultResponse(context *gin.Context) {
 	context.String(http.StatusOK, defaultResponse)
+}
+
+func analyseFile(context *gin.Context) {
+	file, err := context.FormFile("file")
+	// no file received
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "no file received",
+		})
+		return
+	}
+	// generate unique file name for storing
+	fileStorePath := "filestore/" + uuid.New().String() + "_" + file.Filename
+	err = context.SaveUploadedFile(file, fileStorePath)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "unable to save file",
+		})
+	}
 }
