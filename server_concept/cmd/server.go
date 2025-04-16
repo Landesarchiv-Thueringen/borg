53fddf7a1649aeaec7a059e09fa6c2fb1ef56ba9
Tony Grochow <tony.grochow@la.thueringen.de>
2025-04-16 11:50:06 +0200

    init concept server

--- server_concept/cmd/server.go
+++ server_concept/cmd/server.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	VERSION          = "2.0.0"
+	DEFAULT_RESPONSE = "Borg server version %s is running"
+	FILE_STORE_PATH  = "/borg/file-store"
+)
+
+func main() {
+	router := gin.Default()
+	router.MaxMultipartMemory = 5000 << 20 // 5 GiB
+	router.SetTrustedProxies([]string{})
+	router.GET("api", getDefaultResponse)
+	router.GET("api/version", getVersion)
+	router.Run()
+}
+
+func getDefaultResponse(c *gin.Context) {
+	c.String(http.StatusOK, fmt.Sprintf(DEFAULT_RESPONSE, VERSION))
+}
+
+func getVersion(c *gin.Context) {
+	c.String(http.StatusOK, VERSION)
 }
