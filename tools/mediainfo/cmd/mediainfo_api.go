c7e7a5eaf6e344bc161ead50ed56d31ce3e40354
Tony Grochow <tony.grochow@la.thueringen.de>
2025-05-22 16:29:55 +0200

    tools/mediainfo: integration complete

--- tools/mediainfo/cmd/mediainfo_api.go
+++ tools/mediainfo/cmd/mediainfo_api.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,12 +29,33 @@ const (
 	storeDir        = "/borg/file-store"
 )
 
+var toolVersion string
+
 func main() {
+	toolVersion = getToolVersion()
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
 	router.GET("", getDefaultResponse)
 	router.GET("/extract-metadata", extractMetadata)
 	router.Run()
+}
+
+func getToolVersion() string {
+	cmd := exec.Command(
+		"mediainfo",
+		"--Version",
+	)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	outputString := string(output)
+	regEx := regexp.MustCompile(`MediaInfoLib\s*-\s*(.+)`)
+	matches := regEx.FindStringSubmatch(outputString)
+	if len(matches) != 2 {
+		log.Fatal("couldn't extract MediaInfo version from tool output")
+	}
+	return matches[1]
 }
 
 func getDefaultResponse(context *gin.Context) {
@@ -44,7 +69,7 @@ func extractMetadata(context *gin.Context) {
 		log.Println(err)
 		errorMessage := fmt.Sprintf("error processing file: %s", fileStorePath)
 		response := ToolResponse{
-			ToolVersion: "1.223",
+			ToolVersion: toolVersion,
 			Error:       &errorMessage,
 		}
 		context.JSON(http.StatusOK, response)
@@ -52,17 +77,122 @@ func extractMetadata(context *gin.Context) {
 	}
 	cmd := exec.Command(
 		"mediainfo",
+		"--Full",
 		"--Output=XML",
 		fileStorePath,
 	)
 	output, err := cmd.CombinedOutput()
 	outputString := string(output)
-	extractedFeatures := make(map[string]interface{})
+	if err != nil {
+		log.Println(err)
+		errorMessage := fmt.Sprintf(
+			"error executing MediaInfo command: %s", outputString)
+		response := ToolResponse{
+			ToolVersion: toolVersion,
+			Error:       &errorMessage,
+		}
+		context.JSON(http.StatusOK, response)
+		return
+	}
+	features, err := extractFeatures(outputString)
+	if err != nil {
+		errorMessage := err.Error()
+		response := ToolResponse{
+			ToolVersion: toolVersion,
+			Error:       &errorMessage,
+		}
+		context.JSON(http.StatusOK, response)
+		return
+	}
+	mimeType, ok := features["av_container:internetmediatype"]
+	if ok {
+		features["format:mimeType"] = mimeType
+	}
 	response := ToolResponse{
-		ToolVersion:  "1.223",
+		ToolVersion:  toolVersion,
 		ToolOutput:   outputString,
 		OutputFormat: "xml",
-		Features:     extractedFeatures,
+		Features:     features,
 	}
 	context.JSON(http.StatusOK, response)
+}
+
+func extractFeatures(output string) (map[string]interface{}, error) {
+	features := make(map[string]interface{})
+	decoder := xml.NewDecoder(strings.NewReader(output))
+CATEGORY_LOOP:
+	for {
+		token, err := decoder.Token()
+		if err != nil {
+			if err != io.EOF {
+				log.Println(err)
+				return features, err
+			}
+			break
+		}
+		startElement, ok := token.(xml.StartElement)
+		if ok && startElement.Name.Local == "track" {
+			var category string
+			for _, attr := range startElement.Attr {
+				if attr.Name.Local == "type" {
+					category = attr.Value
+					break
+				}
+			}
+			if category == "General" {
+				category = "av_container"
+			}
+			for {
+				innerToken, err := decoder.Token()
+				if err != nil {
+					break
+				}
+				switch innerElement := innerToken.(type) {
+				case xml.StartElement:
+					value, ok := getInnerValue(decoder, innerElement)
+					if !ok {
+						continue
+					}
+					key := fmt.Sprintf(
+						"%s:%s",
+						strings.ToLower(category),
+						strings.ToLower(innerElement.Name.Local))
+					features[key] = value
+				case xml.EndElement:
+					if innerElement.Name.Local == "track" {
+						continue CATEGORY_LOOP
+					}
+				}
+			}
+		}
+	}
+	return features, nil
+}
+
+// getInnerValue returns the value of a flat xml element.
+// ok is false if the element has child elements.
+// Consumes the whole xml element.
+func getInnerValue(decoder *xml.Decoder, element xml.StartElement) (value string, ok bool) {
+	ok = true
+	tagName := element.Name.Local
+	for {
+		nextToken, err := decoder.Token()
+		if err != nil {
+			ok = false
+			break
+		}
+		switch nextElement := nextToken.(type) {
+		case xml.StartElement:
+			ok = false
+		case xml.EndElement:
+			if nextElement.Name.Local == tagName {
+				return
+			}
+		case xml.CharData:
+			if ok {
+				value = string(nextElement)
+			}
+		}
+	}
+	return
 }
